1cde5dc3900b84cdfdbb83b899388b402f93e550
jcwang <jcwang@sonicwall.com>
2022-07-06 22:18:26 +0800

    Add print used by f

---
+++ python_work/apostrophe.py
@@ -0,0 +1,17 @@
+
+msg = "One of Python's strengths is its diverse community."
+
+print(msg)
+
+msg = 'Albert Einstein once said, "A person who never made a mistake never tried anything new."'
+print(msg)
+
+x, y, z = "hello", 5, 5.2
+print(f"x = {x}, y = {y}, z = {z}")
+
+MAX_CONNECTIONS = 5000
+print(MAX_CONNECTIONS)
+MAX_CONNECTIONS = 200
+print(MAX_CONNECTIONS)
+
+print(5+3)
