cfcacceced6f66d70db7882a517f87a6557bf15b
jcwang <jcwang@sonicwall.com>
2022-07-06 22:17:35 +0800

    learn operator to list

---
+++ python_work/listEx.py
@@ -0,0 +1,15 @@
+bicycles = ['12345', 'trek', 'cannondale', 'redline', 'specialized']
+
+print(bicycles)
+
+print(bicycles[-1])
+
+bicycles.append('honda')
+
+print(bicycles[-1])
+
+bicycles.insert(0, "abc")
+print(bicycles)
+
+del bicycles[0]
+print(bicycles)
